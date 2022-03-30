91cb6c51362feaa9aef580968e3761b0032ffba7
VictorNguyen@Chevron.com <VictorNguyen@Chevron.com>
2022-03-29 22:51:55 -0500

    setup for next module

---
+++ Queries/Movie.cs
@@ -0,0 +1,9 @@
+namespace Queries
+{
+	public class Movie
+	{
+		public string Ttile { get; set; }
+		public float Rating { get; set; }
+		public int Year { get; set; }
+	}
+}
--- Queries/Program.cs
+++ Queries/Program.cs
@@ -6,7 +6,15 @@ namespace Queries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!dddd");
+            var movies = new List<Movie>
+            {
+                new Movie { Ttile = "The Dark Knight",      Rating = 8.9f,      Year = 2008 },
+                new Movie { Ttile = "The King's Speech",    Rating = 8.0f,      Year = 2010 },
+                new Movie { Ttile = "Casablanca",           Rating = 8.5f,      Year = 1942 },
+                new Movie { Ttile = "Star Wars V",          Rating = 8.7f,      Year = 1980 }
+            };
+
+            
         }
     }
 }
